7527801a2007f0f769e292edea16057abe26c842
Eliot Fowler <fowlerje@muohio.edu>
2012-04-26 13:23:54 -0400

    created new login php

---
+++ login2.php
@@ -0,0 +1,49 @@
+<?php
+$uname = $_POST['username'];
+$pw = $_POST['password'];
+echo $uname . "<br>";
+echo $pw . "<br>";
+if(!$uname | !$pass)
+		{
+			header("Location: login.php"); 
+		}
+		// checks it against the database
+
+		// This section is errors on incorrect pass
+		//if (!get_magic_quotes_gpc())
+		//{
+		//	$_POST['email'] = addslashes($_POST['email']);
+		//}
+
+		$check = mysql_query("SELECT * FROM users WHERE username = '". $uname ."'") or die(mysql_error());
+
+		//Gives error if user dosen't exist
+		$check2 = mysql_num_rows($check);
+		if ($check2 == 0)
+		{
+			die('That user does not exist in our database. <a href=register.php>Click Here to Register</a>');
+		}
+		while($info = mysql_fetch_array( $check )) 
+		{
+			$pw = stripslashes($_POST['pass']);
+			$info['password'] = stripslashes($info['password']);
+			$md5pw = md5($pw);
+
+			//gives error if the password is wrong
+			if ($md5pw != $info['password'])
+			{
+				die('Incorrect password, please try again.');
+			}
+			else 
+			{ 
+				// if login is ok then we add a cookie 
+				$uname = stripslashes($_POST['username']); 
+				$hour = time() + 3600; 
+				setcookie(ID_my_site, $_POST['username'], $hour); 
+				setcookie(Key_my_site, $_POST['pass'], $hour); 
+
+				//then redirect them to the members area 
+				header("Location: welcome.php"); 
+			} 
+		} 
+?>
