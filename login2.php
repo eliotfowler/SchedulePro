24924d1c820ca08f4b1753a3f04125201c8f130d
Eliot Fowler <fowlerje@muohio.edu>
2012-04-26 14:13:37 -0400

    added email support

--- login2.php
+++ login2.php
@@ -3,55 +3,52 @@
 mysql_connect("localhost", "hackmu", "hackpass") or die(mysql_error()); 
 mysql_select_db("hackmudb") or die(mysql_error()); 
 	
-	
-$uname = $_POST['username'];
+$isuname = false;
+$uname = $_POST['login_name'];
 $pw = $_POST['password'];
-echo "username is " . $uname . "<br>";
+echo "login_name is " . $uname . "<br>";
 echo "password is " . $pw . "<br>";
 if(!$uname | !$pass)
-		{
-			echo "WTF?";
-			//header("Location: login.php"); 
-		}
-		// checks it against the database
+{
+	header("Location: login.php"); 
+}
 
-		// This section is errors on incorrect pass
-		//if (!get_magic_quotes_gpc())
-		//{
-		//	$_POST['email'] = addslashes($_POST['email']);
-		//}
+// checks it against the database
+$check = mysql_query("SELECT * FROM users WHERE username = '". $uname ."'") or die(mysql_error());
 
-		$check = mysql_query("SELECT * FROM users WHERE username = '". $uname ."'") or die(mysql_error());
+//Gives error if user dosen't exist
+$check2 = mysql_num_rows($check);
+if ($check2 == 0)
+{
+	$check = mysql_query("SELECT * FROM users WHERE email = '". $uname ."'") or die(mysql_error());
+	$check3 = mysql_num_rows($check);
+	if($check3 == 0) {
+		header("Location: login.php"); 
+	}
+	
+}
+else $isuname = true;
+while($info = mysql_fetch_array( $check )) 
+{
+	$pw = stripslashes($pw);
+	$info['password'] = stripslashes($info['password']);
+	$md5pw = md5($pw);
 
-		//Gives error if user dosen't exist
-		$check2 = mysql_num_rows($check);
-		if ($check2 == 0)
-		{
-			die('That user does not exist in our database. <a href=register.php>Click Here to Register</a>');
-		}
-		while($info = mysql_fetch_array( $check )) 
-		{
-			$pw = stripslashes($pw);
-			$info['password'] = stripslashes($info['password']);
-			$md5pw = md5($pw);
+	//gives error if the password is wrong
+	if ($md5pw != $info['password'])
+	{
+		header("Location: login.php");
+	}
+	else 
+	{ 
+		// if login is ok then we add a cookie 
+		$uname = stripslashes($_POST['username']); 
+		$hour = time() + 3600; 
+		setcookie(ID_my_site, $_POST['username'], $hour); 
+		setcookie(Key_my_site, $_POST['pass'], $hour); 
 
-			//gives error if the password is wrong
-			if ($md5pw != $info['password'])
-			{
-				echo "md5 pw is " . $md5pw . "<br>";
-				echo "shouldve been " . $info['password'] . "<br>";
-				die('Incorrect password, please try again.');
-			}
-			else 
-			{ 
-				// if login is ok then we add a cookie 
-				$uname = stripslashes($_POST['username']); 
-				$hour = time() + 3600; 
-				setcookie(ID_my_site, $_POST['username'], $hour); 
-				setcookie(Key_my_site, $_POST['pass'], $hour); 
-
-				//then redirect them to the members area 
-				header("Location: welcome.php"); 
-			} 
-		} 
+		//then redirect them to the members area 
+		header("Location: welcome.php"); 
+	} 
+} 
 ?>
