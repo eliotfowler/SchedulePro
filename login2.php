d2b94545dece3da0bdd22aaf8637014dc84bb572
Eliot Fowler <fowlerje@muohio.edu>
2012-04-26 20:27:51 -0400

    trying new registering form

--- login2.php
+++ login2.php
@@ -3,35 +3,33 @@
 mysql_connect("localhost", "hackmu", "hackpass") or die(mysql_error()); 
 mysql_select_db("hackmudb") or die(mysql_error()); 
 	
-$isuname = false;
 $login_name = $_POST['login_name'];
 $pw = $_POST['password'];
-echo "login_name is " . $uname . "<br>";
-echo "password is " . $pw . "<br>";
+
 if(!$uname | !$pass)
 {
 	header("Location: login.php"); 
 }
 
 // checks it against the database
-$check = mysql_query("SELECT * FROM users WHERE username = '". $login_name ."'") or die(mysql_error());
+//$check = mysql_query("SELECT * FROM users WHERE username = '". $login_name ."'") or die(mysql_error());
 
 //Gives error if user dosen't exist
-$check2 = mysql_num_rows($check);
-if ($check2 == 0)
-{
-	$check = mysql_query("SELECT * FROM users WHERE email = '". $login_name ."'") or die(mysql_error());
-	$check3 = mysql_num_rows($check);
-	if($check3 == 0) {
-		header("Location: login.php"); 
-	}
+//$check2 = mysql_num_rows($check);
+//if ($check2 == 0)
+//{
 	
+$check = mysql_query("SELECT * FROM users WHERE email = '". $login_name ."'") or die(mysql_error());
+$check2 = mysql_num_rows($check);
+if($check2 == 0) {
+	header("Location: login.php"); 
 }
-else $isuname = true;
-while($info = mysql_fetch_array( $check )) 
+	
+//}
+while($user_info = mysql_fetch_array( $check )) 
 {
 	$pw = stripslashes($pw);
-	$info['password'] = stripslashes($info['password']);
+	$user_info['password'] = stripslashes($info['password']);
 	$md5pw = md5($pw);
 
 	//gives error if the password is wrong
@@ -41,16 +39,12 @@ while($info = mysql_fetch_array( $check ))
 	}
 	else 
 	{ 
-		if($isuname) {
-			$getemail = mysql_query("SELECT * FROM users WHERE username = '". $login_name ."'") or die(mysql_error());
-			$user_arr = mysql_fetch_array($getemail);
-			$email = $user_arr['email'];
-		}
+		$email = $user_info['email'];
 		// if login is ok then we add a cookie 
 		$email = stripslashes($email); 
 		$hour = time() + 3600; 
 		setcookie(email_cookie, $email, $hour); 
-		setcookie(password_cookie, $info['password'], $hour); 
+		setcookie(password_cookie, $pw, $hour); 
 
 		//then redirect them to the members area 
 		header("Location: welcome.php"); 
