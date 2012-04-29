a9d8d35ba6b8253b5ddb95dd297f1037f98082d1
Eliot Fowler <fowlerje@muohio.edu>
2012-04-29 01:59:42 -0400

    class status

--- check_single_status.php
+++ check_single_status.php
@@ -7,7 +7,8 @@ while(!feof($file))
   {
     $line = split(",",fgets($file));
         if($line[2] - $line[1] <= 0) {
-            mysql_query("INSERT INTO watch_list (uid, crn) VALUES (19, $line[0])");
+            //mysql_query("INSERT INTO watch_list (uid, crn) VALUES (19, $line[0])");
+			echo "user 19 wants to watch the class with crn " . $line[0] . "<br>";
         }
   }
 fclose($file);
