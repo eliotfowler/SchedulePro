d42f1e860af5c72b663c01dd4c36713cbff038e1
Eliot Fowler <fowlerje@muohio.edu>
2012-04-29 21:21:56 -0400

    adding email support if a class opens up

--- wanted.php
+++ wanted.php
@@ -19,9 +19,13 @@ while(!feof($file))
 
 fclose($file);
 foreach ($qresult as $qkey => $qvalue) {
+	echo "<br>checking class " . $qvalue['crn'] . "<BR>";
 	foreach ($pinged as $pkey => $pvalue) {
+	echo "found class " . $pvalue[0] . "<BR>";
 			if($pvalue[0] == $qvalue['crn']) {
+				echo "<br>EQUAL!<br>";
 				if($pvalue[2] - $pvalue[1] > 0 && $qresult['sent'] == 0) {
+					echo "SENDING EMAIL!!!!<br><br><br>;
 					//send email
 					/*
  * Create the body of the message (a plain-text and an HTML version).
