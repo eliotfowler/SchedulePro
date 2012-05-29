969711d9eb8b314639c79e489f524ddeb613a272
Eliot Fowler <fowlerje@muohio.edu>
2012-05-28 21:24:58 -0400

    adding check class base

--- check_class.php
+++ check_class.php
@@ -2,9 +2,32 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Check Class</title>
+<title>Schedule Pro - Check Class</title>
+<link rel="stylesheet" href="css/master.css" type="text/css" media="screen" title="no title" charset="utf-8">
 </head>
-
 <body>
+<div id="checkclass-container">
+    	<div id=userDashboard>
+        	<div id="settings">
+                <a href="#" class="button">
+                    <span class="txt">Settings</span>
+                    <span class="ar">&#9660;</span>
+                </a>
+                <div class="menu">
+                    <ul>
+                        <li><a href="check_single_status.php">Account Settings</a></li>
+                        <li><a href="includes/logout.php">Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="name">
+            	<?php echo "Hello, " . $user_info['fname'] . " " . $user_info['lname']; ?>
+            </div>
+        </div>
+        <div id="check-main">
+        	
+        </div>
+</div>
+        
 </body>
 </html>
