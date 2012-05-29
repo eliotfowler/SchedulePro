688ffb48e15ccba78ea018491620c4226df2a660
Eliot Fowler <fowlerje@muohio.edu>
2012-05-29 16:22:56 -0400

    adding check class base

--- class_search.php
+++ class_search.php
@@ -18,10 +18,10 @@ function checkValues($value)
 	
 	 // Convert all &lt;, &gt; etc. to normal html and then strip these
 	 $value = strtr($value,array_flip(get_html_translation_table(HTML_ENTITIES)));
-	/*
+	
 	 // Strip HTML Tags
 	 $value = strip_tags($value);
-	
+	/*
 	// Quote the value
 	$value = mysql_real_escape_string($value);*/
 	return $value;
