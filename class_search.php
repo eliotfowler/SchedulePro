8638d377d750ecf1175d99ab1fc4dc9564dad8c7
Eliot Fowler <fowlerje@muohio.edu>
2012-05-29 21:42:33 -0400

    working on check class page

--- class_search.php
+++ class_search.php
@@ -27,7 +27,7 @@ function checkValues($value)
 
 $check = mysql_query("SELECT * 
 					  FROM classes, meeting_times 
-					  WHERE dept_code = '$input[0]' AND course_number = '$input[1]' 
+					  WHERE dept_code = '"$input[0]"' AND course_number = '"$input[1]"' 
 					  		AND classes.crn = meeting_times.crn") or die(mysql_error());
 							
 $rows = mysql_fetch_assoc( $check );
